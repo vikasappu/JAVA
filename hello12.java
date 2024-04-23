943baee1cf47f70be9e034af8a5c9bfa7058c9e7
Vikas A <149749470+vikasappu@users.noreply.github.com>
2024-04-23 08:35:27 +0530

    Add files via upload

---
+++ hello12.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class hello12 {
+    public static void main(String[] args) {
+        int x;
+        Scanner vi=new Scanner(System.in);
+        System.out.println("Enter the 2 number");
+        x=vi.nextInt();
+        System.out.println("the sum is"+(2*x*x+2*x+2));
+
+    }
+}
